cfa09475e615b1d2f95580ba488300f6f44af6f5
Y-Nitin <nitinyarlagadda16@gmail.com>
2024-05-27 22:28:32 +0530

    updated Graph.tsx features

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -41,12 +41,15 @@ class Graph extends Component<IProps, {}> {
       elem.setAttribute('view', 'y_line');
       elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
-        timestamp: 'distinct count',
+        price_abc: 'avg',
+      price_def: 'avg',
+      ratio: 'avg',
+      timestamp: 'distinct count',
+      upper_bound: 'avg',
+      lower_bound: 'avg',
+      trigger_alert: 'avg',
       }));
     }
   }
